956f4de35d0b29d8bf9e04a68928a6d6578bc3e6
Momo Eissenhauer <momo.eissenhauer@gmail.com>
2023-04-09 23:57:05 +0200

    add some more buttons that don't work yet

--- zombie/server/queries.py
+++ zombie/server/queries.py
@@ -439,6 +439,7 @@ WHERE players.player_id in (
     SELECT players.player_id
     FROM players
     WHERE players.game_id = %(game_id)s
+    AND NOT players.is_initial_zombie
     ORDER BY random()
     LIMIT coalesce(%(number_zombies)s, 1)
 )
